263f01deb5c0bd0330624df47621919541fe94c0
ulugbek345-MAX <eshnazarovulugbek979@gmail.com>
2020-07-30 00:07:09 +0300

    java scriptni urganyabmn

---
+++ Script/script.js
@@ -0,0 +1,8 @@
+var person = "Abdulla";
+
+    console. log("Abdulla")
+
+
+var  umail = "Java script"; // data type = string;
+var  age = 332;          // data type = string;
+var  isMarride = true;      // data type = boolean;
